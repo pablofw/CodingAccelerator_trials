bf15cba1094c158086fc415e469a677ba86ca65f
Pablo FW <84840632+pablofw@users.noreply.github.com>
2025-02-14 08:55:05 +0100

    Update Terre001.py

--- 1.  Epreuve de la terre/Terre001.py
+++ 1.  Epreuve de la terre/Terre001.py
@@ -11,27 +11,23 @@ print()
 # V2
 # --------------- Utilities --------------- #
 def generate_alphabet():
-    first_ascii = ord("a")
-    last_ascii = ord("z")
+    ascii_a = ord("a")
+    ascii_z = ord("z")
     alphabet = ""
     for ascii_code in range(first_ascii, last_ascii + 1):
         alphabet += chr(ascii_code)
     return alphabet
 
 # --------------- Error handling --------------- #
-def is_valid_alphabet(alphabet):
-    reference_alphabet_FR = "abcdefghijklmnopqrstuvwxyz"
-    return alphabet == reference_alphabet_FR
-
 # --------------- Parsing & Data Retrieval  --------------- #
-def get_alphabet():
-    alphabet = generate_alphabet()
-    return alphabet if is_valid_alphabet(alphabet) else None 
+'''
+no needed, cause no entry in the utilies function
+'''
 
 # --------------- Resolution --------------- #
 def display_alphabet():
-    result = get_alphabet()
-    if result:
+    result = generate_alphabet()
+    if result == "abcdefghijklmnopqrstuvwxyz":
         print(result)
     else:
         print("Error")
