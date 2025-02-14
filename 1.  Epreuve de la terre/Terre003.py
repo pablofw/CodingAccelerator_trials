adcd182eec82b19ab5c81635ab66092cf8eb5301
Pablo FW <84840632+pablofw@users.noreply.github.com>
2025-02-14 15:54:54 +0100

    Update Terre003.py

--- 1.  Epreuve de la terre/Terre003.py
+++ 1.  Epreuve de la terre/Terre003.py
@@ -11,7 +11,7 @@ def get_input_arguments():
     return sys.argv[1:]
 
 # --------------- Resolution --------------- #
-def retrieve_input_sys_arguments():
+def get_validated_input_arguments():
     sys_arguments = get_input_arguments()
     if has_input_arguments(sys_arguments) == False:
         print("Error: No arguments provided.")
@@ -19,9 +19,9 @@ def retrieve_input_sys_arguments():
     else: return arguments
 
 # --------------- Result Display --------------- #
-def display_result():
-    arguments = retrieve_input_sys_arguments()
+def display_inputs_arguments():
+    arguments = get_validated_input_arguments()
     for argument in arguments:
         print(argument)
 
-display_result()
+display_inputs_arguments()
