0488f1e1ed327ceae98dda33889de2abbc2b818f
Pablo FW <84840632+pablofw@users.noreply.github.com>
2025-02-14 09:01:33 +0100

    Update Terre002.py

--- 1.  Epreuve de la terre/Terre002.py
+++ 1.  Epreuve de la terre/Terre002.py
@@ -2,26 +2,25 @@
 '''
 print(__file__.split('/')[-1].split('\\')[-1])
 '''
-
-# Utilities 
+# --------------- Utilities --------------- #
 import sys
 def get_filename():
     full_path = sys.argv[0]
     filename = full_path.split("/")[-1]
     return filename
 
-
-# Error handling 
+# --------------- Error handling --------------- #
 def is_valid_filename(filename):
     return isinstance(filename, str) and len(filename) > 0
 
-# Parsing & Data Retrieval
-# Resolution 
-def find_filename(): #Jtrouve pas d'autre nom de fonction ^^'
+# --------------- Parsing & Data Retrieval--------------- #
+'''no needed'''
+# --------------- Resolution --------------- #
+def find_filename(): 
     filename = get_filename()
     return filename if is_valid_filename(filename) else None
 
-# Result Display
+# --------------- Result Display --------------- #
 def display_filename():
     result = find_filename()
     if result:
