de87fc768e7fb0c96025156d495f8b64924b7575
Pablo FW <84840632+pablofw@users.noreply.github.com>
2025-02-19 13:49:22 +0100

    Update Terre01.py

--- 1.  Epreuve de la terre/Terre01.py
+++ 1.  Epreuve de la terre/Terre01.py
@@ -1,18 +1,21 @@
 # alphabet
 # --------------- Utilities --------------- #
-def get_alphabet():
+def generate_alphabet() -> str:
+    """
+    Generate a string containing the lowercase English alphabet.
+    """
     ascii_a = ord("a")
     ascii_z = ord("z")
     alphabet = ""
     for ascii_code in range(ascii_a, ascii_z + 1):
-        alphabet += chr(ascii_code)
+        alphabet += chr(ascii_code) 
     return alphabet
 
 # --------------- Error handling --------------- #
 # --------------- Parsing & Data Retrieval  --------------- #
 # --------------- Resolution --------------- #
 def display_alphabet():
-    print(get_alphabet())
+    print(generate_alphabet())
 
 # --------------- Result Display / Execution --------------- #
 display_alphabet()
