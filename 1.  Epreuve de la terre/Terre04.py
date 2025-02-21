397bf273fc41adb8310e5db407c28f28be82898b
Pablo FW <84840632+pablofw@users.noreply.github.com>
2025-02-21 17:10:38 +0100

    Update Terre04.py

--- 1.  Epreuve de la terre/Terre04.py
+++ 1.  Epreuve de la terre/Terre04.py
@@ -3,19 +3,14 @@
 Goals:
 - String manipulation
 - Using coherent loops 
+- Discovering index
 - Applying clean code concepts
 '''
 
 # --------------- Utilities --------------- #
 import sys
 
-def get_arguments():
-    """
-    Extract arguments from command line input, without path.
-    """
-    return sys.argv[1:]
-
-def generate_alphabet() -> str:
+def generate_lowercase_alphabet() -> str:
     """
     Generate a string containing the lowercase English alphabet.
     """
@@ -30,38 +25,38 @@ def get_alphabet_from_letter(start_letter: str) -> str:
     """
     Generate the alphabet starting from a specific letter
     """
-    alphabet = generate_alphabet()
-    start_letter_found = False
-    subset_alphabet = ""
-    for letter in alphabet:
-        if letter == start_letter:
-            start_letter_found = True
-        if start_letter_found:
-            subset_alphabet += letter
+    alphabet = generate_lowercase_alphabet()
+    start_index = alphabet.index(start_letter)
 
+    subset_alphabet = ""
+    for i in range (start_index, len(alphabet)):
+        subset_alphabet += alphabet[i]
+    
     return subset_alphabet
 
 # --------------- Error handling --------------- #
 def is_valid_lowercase_letter_argument(arguments: list) -> bool:
+    """
+    Validate that the input is a single lowercase letter.
+    """
     if len(arguments) != 1:
-        print("Error: Please provide only one entry.")
         return False
     start_letter = arguments[0]
     is_single_character = len(start_letter) == 1  
-    is_lowercase_letter = 'a' <= start_letter <= 'z'  
-    if is_single_character and is_lowercase_letter:
-        return True
-    else:
-        print("Error: Please provide a single lowercase letter.")
-        return False 
+    is_lowercase_letter = ('a' <= start_letter <= 'z')
+    return is_single_character and is_lowercase_letter
 
 # --------------- Parsing & Data Retrieval --------------- #
-def get_start_letter():
+def get_arguments():
+    """
+    Extract arguments from command line input, without path.
+    """
+    return sys.argv[1:]
     
 # --------------- Resolution --------------- #
 def process_alphabet_generator_from_letter():
     arguments = get_arguments()
-    if not start_letter or not is_valid_lowercase_letter_argument(start_letter):
+    if not is_valid_lowercase_letter_argument(arguments):
         return None
     start_letter = arguments[0]
     alphabet_extract = get_alphabet_from_letter(start_letter)
